743662067d9114c9d194cb739d54481648872f34
Bence Ádom <github@adombence.hu>
2023-04-14 16:25:07 +0200

    Update Main.java

--- src/main/java/hu/adombence/Main.java
+++ src/main/java/hu/adombence/Main.java
@@ -1,4 +1,6 @@
-package hu.adombence;public class Main {
+package hu.adombence;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
